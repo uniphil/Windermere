9137609d1473232b4218f3bcf396f6fd3cd88602
Philip Schleihauf <uniphil@gmail.com>
2013-07-12 21:33:15 -0400

    mocking data

--- website/endpoints.py
+++ website/endpoints.py
@@ -9,4 +9,17 @@ def home():
 
 @app.route('/overview-sample')
 def mock_overview():
-    return render_template('mock-overview.html')
+    class Content(list):
+        def __init__(self, name, stuff=[]):
+            self.name = name
+            super(Content, self).__init__(stuff)
+
+    contents = [
+        Content("Photos", ["lalalal", "lololo", "lelele"]),
+        Content("Publications", ["one", "two", "threeeee"]),
+        Content("Theses", ["un,", "deux", "troisiemme"]),
+        Content("Presentations", ["blah", "blah", "bleh"]),
+    ]
+            
+    return render_template('mock-overview.html', contents=contents)
+        
