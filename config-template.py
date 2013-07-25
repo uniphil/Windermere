cbf14d3b7fd7d2be9af1bafd9702fd8d84be820b
Philip Schleihauf <uniphil@gmail.com>
2013-07-25 15:33:58 -0400

    template for site configuration

---
+++ config-template.py
@@ -0,0 +1,19 @@
+#
+# config-template.py
+#
+# Copy this file to config.py in this directory, and fill in the blanks
+#
+# This file is part of the Windermere Consortium (Created for Bill Arnott,
+# University of Ottawa Geology Dept)
+#
+
+
+from website import app
+
+
+app.config.update(
+    # make sure the secret key is not guessable!
+    SECRET_KEY='',
+    # database stuff. see here: http://pythonhosted.org/Flask-SQLAlchemy/config.html
+    SQLALCHEMY_DATABASE_URI='',
+)
