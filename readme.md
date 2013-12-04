f911b0f7c1a600886ad0bf124f473ccd0dceb7aa
Phil Schleihauf <uniphil@gmail.com>
2013-12-04 13:40:54 -0500

    xaccel for nginx

--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 DONT'T forget to set secret key env variable bargh...
+
+
+for deployment on nginx, we can d x-accel-redirect
+
+
+`$ gunicorn xaccel:xapp`
---
+++ xaccel.py
@@ -0,0 +1,25 @@
+import os
+root = os.getcwd()
+
+
+class XAccelMiddleware(object):
+    def __init__(self, app):
+        app.config['USE_X_SENDFILE'] = True
+        app.debug = True
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        def _start_response(status, headers, *args, **kwargs):
+            new_headers = [self.xfix(header) for header in headers]
+            return start_response(status, new_headers, *args, **kwargs)
+        return self.app(environ, _start_response)
+
+    def xfix(self, header):
+        if header[0].lower() != 'x-sendfile':
+            return header
+        return ['X-Accel-Redirect', header[1][len(app.root_path):]]
+
+
+
+from website import app
+xapp = XAccelMiddleware(app)
