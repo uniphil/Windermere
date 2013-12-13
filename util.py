fadd83753a8000ba823fe3071e701622603d65b0
Phil Schleihauf <uniphil@gmail.com>
2013-12-13 13:57:12 -0500

    hacky scripts to import the old photos

--- util.py
+++ util.py
@@ -24,6 +24,64 @@ except ImportError:
     print "could not import app..."
 
 
+def details(f):
+    from datetime import datetime
+    from website.models import ScenicPhoto
+
+    for line in f:
+        vals = [v.strip() for v in line.split('\t')]
+        photo = ScenicPhoto()
+        photo.photo = os.path.join(vals[1])
+        photo.added = datetime.fromtimestamp(int(vals[2]))
+        photo.title = vals[3]
+        photo.description = vals[4]
+        photo.featured = False
+        yield photo
+
+
+def old_photos_import():
+    from website.models import db
+    deets = open('photos.csv')
+    deets.readline()  # skip header
+    for photo in details(deets):
+        db.session.add(photo)
+    db.session.commit()
+
+
+def resize_old_photos():
+    from PIL import Image
+    f = open('photos.csv')
+    f.readline()  # skip header
+    for line in f:
+        path = app.config['scenic'](line.split('\t')[1].strip())
+        try:
+            im = Image.open(path)
+            print '.',
+        except IOError as e:
+            print
+            print e
+            continue
+        im.thumbnail((1140, 1140), Image.ANTIALIAS)
+        im.save(path + '_sized.jpg', 'JPEG')
+        im.thumbnail((256, 256), Image.ANTIALIAS)
+        im.save(path + '_small.jpg', 'JPEG')
+        del im
+
+
+def clean_dead_photos():
+    from website.models import db, ScenicPhoto
+
+    for photo in ScenicPhoto.query.all():
+        exists = True
+        try:
+            i = open(app.config['scenic'](photo.photo))
+        except IOError:
+            print 'x'
+            db.session.delete(photo)
+    db.session.commit()
+
+
+
 if __name__ == '__main__':
     import sys
     cmd = sys.argv[1]
@@ -71,6 +129,16 @@ if __name__ == '__main__':
                 if e.errno != 17:
                     raise e
                 print '    {}: {}'.format(path, e.strerror)
+
+    elif cmd == 'import_photos':
+        old_photos_import()
+
+    elif cmd == 'resize_old_photos':
+        resize_old_photos()
+
+    elif cmd == 'clean_dead_photos':
+        clean_dead_photos()
+
 
     elif cmd == 'server':
         #from xaccel import app
