57458330ea6872849a27961c6f46c52594c7ed54
Phil Schleihauf <uniphil@gmail.com>
2014-02-20 13:41:11 -0500

    db init stuff

--- manage.py
+++ manage.py
@@ -70,32 +70,28 @@ def load_olddocs(datafile='import-old/data.json'):
     db.session.commit()
 
 
+@manager.command
+def seed_key():
+    from website.models import db, Partner
+    from datetime import datetime
+    partner = Partner()
+    partner.name = 'Consortium Partner'
+    partner.key = app.config['ACCESS_SEED']
+    partner.last_keychange = datetime.now()
+    db.session.add(partner)
+    db.session.commit()
+
+
+@manager.command
+def mkphil():
+    from website import models
+    phil = models.Admin()
+    phil.name = 'Phil Schleihauf'
+    phil.username = 'phil'
+    phil.set_password('asdf')
+    models.db.session.add(phil)
+    models.db.session.commit()
+
+
 if __name__ == '__main__':
     manager.run()
-
-
-    # elif cmd in ['initdb', 'reinitdb']:
-    #     from website.models import db, Partner
-    #     from datetime import datetime
-    #     if cmd == 'reinitdb':
-    #         print 'dropping db'
-    #         db.drop_all()
-    #     print 'creating db'
-    #     db.create_all()
-    #     print 'seeding'
-    #     partner = Partner()
-    #     partner.name = 'Consortium Partner'
-    #     partner.key = app.config['ACCESS_SEED']
-    #     partner.last_keychange = datetime.now()
-    #     db.session.add(partner)
-    #     db.session.commit()
-
-
-    # elif cmd == 'mkphil':
-    #     from website import models
-    #     phil = models.Admin()
-    #     phil.name = 'Phil Schleihauf'
-    #     phil.username = 'phil'
-    #     phil.set_password('asdf')
-    #     models.db.session.add(phil)
-    #     models.db.session.commit()
